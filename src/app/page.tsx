9c21705994312203e6d87280b4bb2169807852c5
Philip Rasmusson <rasmussonphilip@gmail.com>
2023-08-04 17:10:08 +0200

    sanity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Posts from '@/app/_components/Posts'
 import { cachedClient } from '../../sanity/lib/client'
 import { postsQuery } from '../../sanity/lib/queries'
+// comment
 
 export default async function Home() {
   const posts = await cachedClient(postsQuery)
