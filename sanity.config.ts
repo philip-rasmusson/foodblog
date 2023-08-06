87395c01e8fbe5746ccb6401c5d76479cd969b89
Philip Rasmusson <rasmussonphilip@gmail.com>
2023-08-06 08:17:40 +0200

    vercel deploy tool

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,7 @@
 
 import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
 import { deskTool } from 'sanity/desk'
 import { defaultDocumentNode } from './sanity/desk/defaultDocumentNode'
 
@@ -19,7 +20,7 @@ export default defineConfig({
   schema,
   plugins: [
     deskTool({ defaultDocumentNode }),
-    deskTool(),
+    vercelDeployTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
